b704672f1ae4431ed5a3f30e7ecfb97bd4bc1b77
yoo4471 <bell1346@gmail.com>
2017-05-11 20:29:14 +0900

    Add top_menu and add folder that name is views/partials

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/menu', function(req, res, next) {
+  res.render('menu', { title: 'Menu GOOOD' });
+});
 module.exports = router;
