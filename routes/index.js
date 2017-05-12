61bbc5f1a370a728c05a6e8681e8ec59f20902d4
liil93 <liil93@github.com>
2017-05-12 22:33:27 +0900

    modify css

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-/* GET home page. */
-router.get('/menu', function(req, res, next) {
-  res.render('menu', { title: 'Menu GOOOD' });
-});
 module.exports = router;
