69eb441985fd04210d7293a8cb52c349c3db758b
yoo4471 <bell1346@gmail.com>
2017-05-18 02:27:45 +0900

    mongoose 설치 및 data_table 영화 데이터에 맞게 제공

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var MovieContents = require('../models/movieSchema');
 var router = express.Router();
 
 /* GET home page. */
@@ -18,9 +19,60 @@ router.get('/test', function(req, res, next) {
   res.render('flatUI/form');
 });
 
+/* GET users listing. */
+router.get('/all',function(req, res, next) {
+
+  MovieContents.find(function(err, movieContents){
+      if(err) return res.status(500).send({error: 'database failure'});
+
+      var data = []
+      var current = ""
+      for (i = 0; i < movieContents.length; i++) {
+        if (movieContents[i]['current'] == "1") { current = "상영중"}
+        else current = ""
+        movielist = [movieContents[i]['title_kor'], movieContents[i]['title_eng'], movieContents[i]['nation'],
+        movieContents[i]['director'], movieContents[i]['relese_date'], current, movieContents[i]['_id']]
+        data.push(movielist)
+
+
+      }
+      console.log(data)
+
+      var a = {"data" : data}
+      res.json(a);
+  })
+
+
+
+});
 
 router.post('/movielist/enrollment',function(req,res,next){
+
   console.log(req.body);
+  // creates DB schema
+
+  // compiels our schema into a model
+  // var User = mongoose.model('User', boardSchema);
+
+  var newMovieContents = new MovieContents;
+
+  newMovieContents.title_kor = req.body.title_kor;
+  newMovieContents.title_eng = req.body.title_eng;
+  newMovieContents.nation = req.body.nation;
+  newMovieContents.relese_date = req.body.relese_date;
+  newMovieContents.run_time = req.body.run_time;
+  newMovieContents.grade = req.body.grade;
+  newMovieContents.director = req.body.director;
+  newMovieContents.actors = req.body.actors;
+  newMovieContents.description_title = req.body.description_title;
+  newMovieContents.description = req.body.description;
+  newMovieContents.current = req.body.current;
+
+
+  newMovieContents.save(function (err)  {
+    if (err) throw err;
+    console.log('success join')
+  });
   // creates DB schema
 
   // compiels our schema into a model
